f36909ad37ff0452700e0b3f814ba6a96006ef02
PSALMDATA <samuelprinciple1@gmail.com>
2026-07-23 08:52:42 +0100

    Add files via upload

---
+++ courses/GST102/questions.js
@@ -0,0 +1,474 @@
+QUESTIONS = [
+  {
+    "question": "A topic sentence usually relates to the central theme of the _____________",
+    "options": {
+      "A": "Write up",
+      "B": "essay",
+      "C": "report",
+      "D": "paragraph"
+    },
+    "answer": "D",
+    "type": "mcq"
+  },
+  {
+    "question": "The only way you can learn how to summarise a given passage is for you to ______",
+    "options": {
+      "A": "listen carefully to a summary lecture",
+      "B": "Stylishly copy your colleague\u2019s work when no one is watching",
+      "C": "do it practically",
+      "D": "learn it off-hand"
+    },
+    "answer": "C",
+    "type": "mcq"
+  },
+  
+  {
+    "question": "Beginning a paragraph with ________ helps both the reader and the writer understand clearly what the paragraph is abou",
+    "options": {
+      "A": "Generalisations",
+      "B": "examples",
+      "C": "illustrations",
+      "D": "a topic sentence"
+    },
+    "answer": "D",
+    "type": "mcq"
+  },
+  {
+    "question": "Explaining ________ of a concept or thing is called definition.",
+    "options": {
+      "A": "the make up",
+      "B": "the illustration",
+      "C": "the context",
+      "D": "the meaning"
+    },
+    "answer": "D",
+    "type": "mcq"
+  },
+  {
+    "question": "_________ is the logical transition from one idea to the next in a given paragraph.",
+    "options": {
+      "A": "Transition",
+      "B": "Cohesion",
+      "C": "Linguistic expression",
+      "D": "Logical movement"
+    },
+    "answer": "B",
+    "type": "mcq"
+  },
+  {
+    "question": "________in a piece of writing refers to the logical transition from one idea to the next.",
+    "options": {
+      "A": "Coherence",
+      "B": "Practical transition",
+      "C": "Gradual transition",
+      "D": "Critical transition"
+    },
+    "answer": "A",
+    "type": "mcq"
+  },
+  {
+    "question": "_______ are often unified by a central theme.",
+    "options": {
+      "A": "Similar phrases in a given sentence",
+      "B": "Different sentence structures",
+      "C": "Different paragraphs",
+      "D": "Different clause patterns in a sentence"
+    },
+    "answer": "C",
+    "type": "mcq"
+  },
+  {
+    "question": "The main idea is usually couched in the",
+    "answer": "Topic sentence",
+    "type": "fill"
+  },
+  {
+    "question": "Most topic sentences are usually ______ of the paragraph",
+    "answer": "at the beginning ",
+    "type": "fill"
+  },
+  {
+    "question": "In most writings, the _____ in a paragraph tends to present the central theme in it.",
+    "answer": "first sentence",
+    "type": "fill"
+  },
+  {
+    "question": "The central concern of narrative writing is",
+    "answer": "telling a story",
+    "type": "fill"
+  },
+  
+  {
+    "question": "It has been observed that students who want to be __________, need to learn how to summarise.",
+    "options": {
+      "A": "well-grounded in the act of plagiarism",
+      "B": "well-grounded in their speaking skills",
+      "C": "well-focused in their writing",
+      "D": "excellent in their debating skills"
+    },
+    "answer": "C",
+    "type": "mcq"
+  },
+  {
+    "question": "In every piece of writing, the ________should relate to the central theme of the paragraph.",
+    "options": {
+      "A": "Supporting ideas",
+      "B": "Main points",
+      "C": "Relevant issues",
+      "D": "topic sentence"
+    },
+    "answer": "D",
+    "type": "mcq"
+  },
+  
+  {
+    "question": "Placing a topic sentence at the end of a paragraph _________",
+    "options": {
+      "A": "helps both the reader and the writer",
+      "B": "allows for an effective summary",
+      "C": "allows for all the sentences to be built up to the topic",
+      "D": "makes the paragraph more effective"
+    },
+    "answer": "C",
+    "type": "mcq"
+  },
+  {
+    "question": "It is a grammatical fact that a topic sentence cannot be _________.",
+    "options": {
+      "A": "a statement",
+      "B": "An interrogative utterance",
+      "C": "Complex-compound sentence",
+      "D": "Complex sentence"
+    },
+    "answer": "B",
+    "type": "mcq"
+  },
+  {
+    "question": "The word DEFINITION can mean ____________________",
+    "options": {
+      "A": "analysing the meaning of a concept or thing",
+      "B": "explaining the meaning of a concept or thing",
+      "C": "identifying the meaning of a concept or thing",
+      "D": "illustrating the meaning of a concept or thing"
+    },
+    "answer": "B",
+    "type": "mcq"
+  },
+  {
+    "question": "Expressions like 'first, second, third, ' indicate _________within sentences",
+    "options": {
+      "A": "sequence",
+      "B": "comparison",
+      "C": "result",
+      "D": "contrast"
+    },
+    "answer": "A",
+    "type": "mcq"
+  },
+  {
+    "question": "One of the following words or phrases expresses additional information__________",
+    "options": {
+      "A": "besides",
+      "B": "likewise",
+      "C": "consequently",
+      "D": "next"
+    },
+    "answer": "A",
+    "type": "mcq"
+  },
+  {
+    "question": "The main idea is usually couched in the",
+    "answer": "Topic sentence",
+    "type": "fill"
+  },
+  {
+    "question": "The topic sentence in a paragraph often leads to the achievement of ______ in the given composition itself.",
+    "options": {
+      "A": "transitional devices",
+      "B": "logical transitions",
+      "C": "coherence",
+      "D": "supporting details"
+    },
+    "answer": "C",
+    "type": "mcq"
+  },
+  {
+    "question": "When writing, which of the following may not help you achieve coherence in your paragraphs?_______",
+    "options": {
+      "A": "Transitional devices",
+      "B": "Logical transitions",
+      "C": "Topic sentence and supporting details",
+      "D": "topic sentence"
+    },
+    "answer": "C",
+    "type": "mcq"
+  },
+  {
+    "question": "As far as the topic sentence is concerned, there is often more than one in a paragraph. This sentence is _______________",
+    "options": {
+      "A": "probable",
+      "B": "Very likely, but it depends on the paragraph structure",
+      "C": "Right and can be proved",
+      "D": "Wrong and cannot be ascertained"
+    },
+    "answer": "D",
+    "type": "mcq"
+  },
+  {
+    "question": "The word dancing in \u201cDancing is fun\u201d is a _____.",
+    "answer": "gerund",
+    "type": "fill"
+  },
+  
+  {
+    "question": "Words or phrases that connect ideas smoothly in writing are called _____.",
+    "answer": "transitional devices",
+    "type": "fill"
+  },
+  {
+    "question": "Giving examples to explain a point is known as the method of paragraph development by _____.",
+    "answer": "illustration",
+    "type": "fill"
+  },
+  {
+    "question": "Writing that paints a picture in the reader\u2019s mind is paragraph development by _____.",
+    "answer": "description",
+    "type": "fill"
+  },
+  {
+    "question": "Paragraphs can also be developed by showing causes and _____.",
+    "answer": "effects",
+    "type": "fill"
+  },
+  {
+    "question": "When two ideas or objects are examined for similarities, the method used is _____.",
+    "answer": "comparison",
+    "type": "fill"
+  },
+  {
+    "question": "Placing a topic sentence at the end of a paragraph ________",
+    "options": {
+      "A": "allows for all the sentences to be built up to the topic",
+      "B": "helps both the reader and the writer",
+      "C": "makes the paragraph more effective",
+      "D": "allows for an effective summary"
+    },
+    "answer": "A",
+    "type": "mcq"
+  },
+  {
+    "question": "The word \u2018however\u2019 is often used in a write up to express ________",
+    "options": {
+      "A": "functionality",
+      "B": "consequences",
+      "C": "contrast",
+      "D": "continuity"
+    },
+    "answer": "C",
+    "type": "mcq"
+  },
+  {
+    "question": "_________ is a phrase that helps the writer to express result in a writing",
+    "options": {
+      "A": "Similarly",
+      "B": "Accordingly",
+      "C": "Yet",
+      "D": "Furthermore"
+    },
+    "answer": "B",
+    "type": "mcq"
+  },
+  {
+    "question": "The sentence \u201cOn the contrary, I see the idea as one that will divide us\u201d expresses _______",
+    "options": {
+      "A": "a temporal relationship",
+      "B": "Idealistic issues",
+      "C": "a contrast",
+      "D": "The sense of sight"
+    },
+    "answer": "C",
+    "type": "mcq"
+  },
+  {
+    "question": "The word \u2018besides\u2019 can be used to express ________ in a write up.",
+    "options": {
+      "A": "additional information",
+      "B": "Synonymous information",
+      "C": "The first piece of information",
+      "D": "Opposing information"
+    },
+    "answer": "A",
+    "type": "mcq"
+  },
+  {
+    "question": "___________in paragraphs help the reader to understand difficult concepts presented in the composition",
+    "options": {
+      "A": "Metaphorical expressions",
+      "B": "Contextual expressions",
+      "C": "Ironical expressions",
+      "D": "Illustrations"
+    },
+    "answer": "D",
+    "type": "mcq"
+  },
+  
+  {
+    "question": "The fear of speaking before an audience is known as _____.",
+    "answer": "stage fright",
+    "type": "fill"
+  },
+  {
+    "question": "The words a, an, and the are known as _____.",
+    "answer": "articles",
+    "type": "fill"
+  },
+  {
+    "question": "A and an are called _____ articles.",
+    "answer": "indefinite",
+    "type": "fill"
+  },
+  {
+    "question": "The agreement between a subject and its verb in number and person is called _____.",
+    "answer": "concord",
+    "type": "fill"
+  },
+  
+  {
+    "question": "Which of the following is NOT a reason for comparison and contrast in paragraph development? _________",
+    "options": {
+      "A": "to show the superiority of one thing over another",
+      "B": "to identify issues in paragraphs",
+      "C": "to point out distinctions when giving information on two things",
+      "D": "to clarify the unfamiliar by comparing it with the familiar"
+    },
+    "answer": "B",
+    "type": "mcq"
+  },
+  {
+    "question": "The word \u2018subsequently\u2019 can be used to express ________ in a write up.",
+    "options": {
+      "A": "Consequential actions",
+      "B": "time",
+      "C": "Nonsensical expressions",
+      "D": "Idiomatic usage"
+    },
+    "answer": "B",
+    "type": "mcq"
+  },
+  {
+    "question": "The topic sentence in a paragraph often leads to the achievement of ______ in the given composition itself.",
+    "options": {
+      "A": "coherence",
+      "B": "supporting details",
+      "C": "transitional devices",
+      "D": "logical transitions"
+    },
+    "answer": "A",
+    "type": "mcq"
+  },
+  {
+    "question": "Beginning a paragraph with ________ helps both the reader and the writer understand clearly what the paragraph is about",
+    "options": {
+      "A": "a topic sentence",
+      "B": "illustrations",
+      "C": "examples",
+      "D": "Generalisations"
+    },
+    "answer": "A",
+    "type": "mcq"
+  },
+  {
+    "question": "'On the contrary\u00a0I see the idea as one that will divide us'. The underlined word in the sentence indicates _____",
+    "options": {
+      "A": "a contrast",
+      "B": "a comparison",
+      "C": "an example",
+      "D": "a temporal relationship"
+    },
+    "answer": "A",
+    "type": "mcq"
+  },
+  {
+    "question": "The core aspect of a paragraph is its",
+    "answer": "Main idea",
+    "type": "fill"
+  },
+  {
+    "question": "Most topic sentences are usually ______ of the paragraph",
+    "answer": "at the beginning ",
+    "type": "fill"
+  },
+  {
+    "question": "In most writings, the _____ in a paragraph tends to present the central theme in it.",
+    "answer": "first sentence",
+    "type": "fill"
+  },
+  
+  {
+    "question": "An essay you write to present your class debate will be written in _____ language.",
+    "answer": "formal",
+    "type": "fill"
+  },
+  {
+    "question": "Most letters to the editor tend to contain",
+    "answer": "complaints",
+    "type": "fill"
+  },
+  {
+    "question": "If you witnessed an armed robbery incident, the report you will give to the Police is called",
+    "answer": "a statement",
+    "type": "fill"
+  },
+  
+  {
+    "question": "Writers are often encouraged to use adequate illustrations in paragraphs because they ___",
+    "options": {
+      "A": "help both writer and reader understand each other",
+      "B": "help the writer to create an accurate image of an event",
+      "C": "help the reader to understand difficult concepts",
+      "D": "help the writer to explain reasons for an event"
+    },
+    "answer": "C",
+    "type": "mcq"
+  },
+  {
+    "question": "In the sentence, 'She was always out of school during the weekends in her undergraduate days' the tense form used is_______",
+    "options": {
+      "A": "past continuous",
+      "B": "simple past tense",
+      "C": "past tense",
+      "D": "simple tense"
+    },
+    "answer": "B",
+    "type": "mcq"
+  },
+  {
+    "question": "It is often recommended that writers use descriptions in paragraphs because they _____",
+    "options": {
+      "A": "help the writer to create an accurate image of an event",
+      "B": "the writer to explain reasons for an event",
+      "C": "help both writer and reader appreciates the issues discussed in the paragraph",
+      "D": "help the reader to understand difficult concepts"
+    },
+    "answer": "A",
+    "type": "mcq"
+  },
+  
+  {
+    "question": "Which is the appropriate present perfect tense form needed to complete the sentence 'The fishermen _______ fishing all night long'?",
+    "options": {
+      "A": "have been",
+      "B": "would have been",
+      "C": "could have been",
+      "D": "Should have been"
+    },
+    "answer": "A",
+    "type": "mcq"
+  },
+  {
+    "question": "A good summary should be brief, clear, and free from _____.",
+    "answer": "personal opinion",
+    "type": "fill"
+  }
+  
+]
